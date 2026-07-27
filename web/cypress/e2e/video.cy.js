7d12fafa91439c1071e9d1f6487fc5a0de071f2c
Cristiano da Silva Ferreira <cristianodevsystem@gmail.com>
2026-07-27 20:28:00 -0300

    feat: add video playback test with iframe interaction

---
+++ web/cypress/e2e/video.cy.js
@@ -0,0 +1,18 @@
+describe("Tocar vídeo", () => {
+  it("Deve pode tocar o vídeo de exemplo", () => {
+    cy.startPage();
+    cy.submitLoginForm("papito@webdojo.com", "katana123");
+
+    cy.contains("Video").click();
+
+    cy.get("iframe[title='Video Player']")
+      .should("exist")
+      .its("0.contentDocument.body")
+      .then(cy.wrap)
+      .as("iFramePlayer");
+
+    cy.get("@iFramePlayer").find(".play-button").click();
+
+    cy.get("@iFramePlayer").find(".pause-button").should("be.visible");
+  });
+});
